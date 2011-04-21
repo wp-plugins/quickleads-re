b90d204aed480439ea62a6dbdb4ec54ade98f0a1
bdclimber14 <bdclimber14@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-21 05:10:25 +0000

    Version .01
    git-svn-id: https://plugins.svn.wordpress.org/quickleads-re/trunk@375478 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ authorize.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+require_once('load-wp.php');
+require_once('twitteroauth/twitteroauth.php');
+require_once('config.php');
+// Get connection to Twitter with temporary credentials
+$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
+$request_token = $connection->getRequestToken(OAUTH_CALLBACK);
+
+/* Save temporary credentials to session. */
+$_SESSION['oauth_token'] = $token = $request_token['oauth_token'];
+$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+/* If last connection failed don't display authorization link. */
+
+switch ($connection->http_code) {
+  case 200:
+    /* Build authorize URL and redirect user to Twitter. */
+    $url = $connection->getAuthorizeURL($token);
+    header('Location: ' . $url); 
+    break;
+  default:
+    /* Show notification if something went wrong. */
+    echo 'Could not connect to Twitter. Refresh the page or try again later.';
+}
